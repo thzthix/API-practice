c68e206270d561fef5aa4f3c306008c24cacdbc7
thzthix <95135403+thzthix@users.noreply.github.com>
2023-04-29 22:14:16 +0900

    Create script.js

---
+++ randomQuote/script.js
@@ -0,0 +1,18 @@
+const quote = document.querySelector(".quote");
+const author = document.querySelector(".author");
+const getBtn = document.getElementById("get-btn");
+
+const getQuote = async () => {
+  quote.classList.remove("fade");
+  author.classList.remove("fade");
+
+  const response = await fetch("https://api.quotable.io/random");
+  const json = await response.json();
+  quote.textContent = json.content;
+  author.textContent = json.author;
+
+  quote.classList.add("fade");
+  author.classList.add("fade");
+};
+
+getBtn.addEventListener("click", getQuote);
