4ffda876d7d31600b146a935fc3141c28d07858a
thzthix <95135403+thzthix@users.noreply.github.com>
2023-05-05 12:52:30 +0900

    Update script.js

--- NumberTrivia/script.js
+++ NumberTrivia/script.js
@@ -2,22 +2,33 @@ const GET_URL = "http://numbersapi.com/";
 const GET_RANDOM_URL = "http://numbersapi.com/random/trivia?json";
 const searchForm = document.getElementById("search-box");
 const getRandBtn = document.getElementById("get-random-fact-btn");
+const resultContainer = document.getElementById("result-container");
+
+const sketchresult = (number, description) => {
+  resultContainer.innerHTML = `        
+  <h3 id="name">${number}</h3>
+  <p id="description-p">${description}</p>`;
+};
 const getFact = async (event) => {
   event.preventDefault();
   try {
     const inputValue = document.getElementById("search-input").value;
-    if (inputValue > 300) {
-      console.log("please enter a number between 0 to 300");
+    if (inputValue.trim() === "") {
+      resultContainer.innerHTML = `<p class="message"> The input field cannot be empty.</p>`;
+      return;
+    } else if (
+      parseInt(inputValue) > 300 ||
+      parseInt(inputValue) < 0 ||
+      typeof inputValue.trim() !== Number
+    ) {
+      resultContainer.innerHTML = `<p class="message"> Please enter a number between 0 to 300.</p>`;
+
       return;
     }
     const response = await fetch(`${GET_URL}${inputValue}`);
     const text = await response.text();
 
-    const name = document.getElementById("name");
-
-    const description = document.getElementById("description-p");
-    name.textContent = inputValue;
-    description.textContent = text;
+    sketchresult(inputValue, text);
     console.log(json);
   } catch {
     const inputValue = document.getElementById("search-input").value;
@@ -29,12 +40,7 @@ const getRandom = async (event) => {
   try {
     const response = await fetch(GET_RANDOM_URL);
     const json = await response.json();
-
-    const name = document.getElementById("name");
-
-    const description = document.getElementById("description-p");
-    name.textContent = json.number;
-    description.textContent = json.text;
+    sketchresult(json.number, json.text);
     console.log(json);
   } catch {
     const inputValue = document.getElementById("search-input").value;
